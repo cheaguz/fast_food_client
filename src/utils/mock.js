9d180c9a37872f91fc3baa3f79174be9009073ec
Cheaguz <aguzf83@gmail.com>
2022-06-05 16:08:58 -0300

    first commit

---
+++ src/utils/mock.js
@@ -0,0 +1,24 @@
+export const mock =[
+    {
+        category: "minutas",
+        comments: "Con aceitunas",
+        createdAt: null,
+        description: "Pizza muzarella de la casa",
+        id: 1,
+        image: "https://www.clarin.com/img/2018/12/11/la-pizza-de-muzzarella-un___trqq9bkXM_340x340__1.jpg",
+        name: "Pizza muzzarella",
+        price: 700,
+        updatedAt: null,
+    },
+    {
+        category: "minutas",
+        comments: "la mejor pizza de don cangrejo",
+        createdAt: "2022-05-30T04:58:34.000Z",
+        description: "Pizza especial , muzzarella con morron y jamon",
+        id: 2,
+        image: "https://img-global.cpcdn.com/recipes/dfdebcfc818be032/1200x630cq70/photo.jpg",
+        name: "Pizza especial",
+        price: 900,
+        updatedAt: "2022-05-30T04:58:34.000Z",
+    }
+]
